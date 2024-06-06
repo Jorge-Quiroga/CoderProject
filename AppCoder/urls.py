9a476ebef39c9a7c953b0f0ee464f6a5f59419e5
Jorge Quiroga <sh0keh@live.com.ar>
2024-06-05 22:21:13 -0300

    cambios actualizados para la tercera pre Entrega

---
+++ AppCoder/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from AppCoder.views import curso, lista_cursos, inicio, cursos, profesores, estudiantes, entregables
+
+urlpatterns = [
+    path('agrega-curso/<nombre>/<camada>', curso),
+    path('lista-cursos/', lista_cursos),
+    path('', inicio, name='Inicio'),
+    path('cursos/', cursos, name='Cursos'),
+    path('profesores/', profesores, name='Profesores'),
+    path('estudiantes/', estudiantes, name='Estudiantes'),
+    path('entregables/', entregables, name='Entregables'),
+]
